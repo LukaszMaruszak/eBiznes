01a9a10d288247fc543590c79dd6f115e57d6d2b
Łukasz Maruszak <64308973+LukaszMaruszak@users.noreply.github.com>
2022-06-04 14:28:26 +0200

    Refactor to remove SonarCloud issues

--- zadanie7/frontend/src/components/Login/Login.js
+++ zadanie7/frontend/src/components/Login/Login.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { GithubLoginButton } from "react-social-login-buttons";
-import { GoogleLoginButton } from "react-social-login-buttons";
+import { GithubLoginButton, GoogleLoginButton } from "react-social-login-buttons";
 import axios from "../../config/AxiosConfig";
 
 function Login() {
 
     const redirectToGoogleSSO = async () => {
         axios.get("/google/login").then((googleLoginURL) => {
-            console.log(googleLoginURL.data)
-            const newWindow = window.open(googleLoginURL.data, "_self");
+            console.log(googleLoginURL.data);
+            window.open(googleLoginURL.data, "_self");
         });
     }
 
     const redirectToGitHubSSO = async () => {
         axios.get("/github/login").then((githubLoginURL) => {
-            console.log(githubLoginURL.data)
-            const newWindow = window.open(githubLoginURL.data, "_self");
+            console.log(githubLoginURL.data);
+            window.open(githubLoginURL.data, "_self");
         });
     }
 
